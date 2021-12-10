b3b58831ccb796c19abb5d347a8b44099f67c813
iddimsangi <adammsangi@gmail.com>
2021-12-10 21:16:15 +0300

    complete card list footer

--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,10 +1,22 @@
 import React from 'react'
+import './TodoList.scss'
 import TodoCard from './TodoCard/TodoCard'
 function TodoList() {
     return (
-        <div>
+        <div className="cardList">
             {/* <h1>list</h1> */}
             <TodoCard/>
+            <TodoCard/>
+            <TodoCard/>
+            <div className="list-footer">
+                <a href="#">3itemlefts</a>
+                <div>
+                    <a href="#">All</a>
+                    <a href="#">Active</a>
+                    <a href="#">Completed</a>
+                </div>
+                <a href="#">ClearCompleted</a>
+            </div>
         </div>
     )
 }
