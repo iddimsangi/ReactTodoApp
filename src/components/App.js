af48d9ba050de0d1906d2141de4d01e7347c97a9
iddimsangi <adammsangi@gmail.com>
2021-12-11 17:26:23 +0300

    loggle toggle fixed

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,22 @@
 import sun from "../images/icon-sun.svg";
+import moon from "../images/icon-moon.svg"
 import "./App.scss";
 import AddTodo from "./AddTodo/AddTodo";
 import TodoList from "./TodoList/TodoList";
 function App() {
   return (
     <div className="App">
+      <input type="checkbox" className="checkboxIn" id="logoToggle"/>
       <div className="App-container">
         <div className="App-container--main">
           <div className="App-container--main-head">
             <h1>todo</h1>
+        
+            <label htmlFor="logoToggle">
             <img src={sun} alt="sun-logo" />
+            <img className="icon-moon" src={moon} alt="sun-logo" />
+            </label>
+           
           </div>
           <div className="App-container--main-body">
             <AddTodo />
