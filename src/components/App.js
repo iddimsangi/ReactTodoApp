36533e6ab79e72b1e6b54058b715c264a5631696
iddimsangi <adammsangi@gmail.com>
2021-12-13 11:58:33 +0300

    add todos state in a local storage

--- src/components/App.js
+++ src/components/App.js
@@ -1,51 +1,55 @@
-import { useState } from 'react'
+import { useState, useEffect } from "react";
 import sun from "../images/icon-sun.svg";
-import moon from "../images/icon-moon.svg"
+import moon from "../images/icon-moon.svg";
 import "./App.scss";
 import AddTodo from "./AddTodo/AddTodo";
+import uniqid from "uniqid";
 import TodoList from "./TodoList/TodoList";
 function App() {
-  const[todos, setTodos] = useState([]);
-  const AddTodoHandler = (todo) =>{
-    setTodos([
-      {ID: new Date(), ...todo},
-      ...todos
-    ])
-  }
-  const deleteIdHandler = (Id) =>{
-    const deletedTodo = todos.filter(todo =>{
-      return todo.ID !== Id
-    })
-    console.log(deletedTodo)
-    setTodos([deletedTodo])
-  }
-  // console.log(todos)
+  const LOCAL_STORAGE_KEY = "todos";
+  const [todos, setTodos] = useState([]);
+  const AddTodoHandler = (todo) => {
+    setTodos([{ ID: uniqid(), ...todo }, ...todos]);
+  };
+  const deleteIdHandler = (Id) => {
+    const deletedTodo = todos.filter((todo) => {
+      return todo.ID !== Id;
+    });
+    setTodos(deletedTodo);
+  };
+  useEffect(() =>{
+   const receivedTodos = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
+   if(receivedTodos) setTodos(receivedTodos)
+      },[])
+  useEffect(() =>{
+localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(todos));
+  },[todos])
+  console.log(todos);
   return (
-<div>
-<input type="checkbox" className="checkboxIn" id="logoToggle"/>
-<div className="App">
-      <div className="App-container">
-        <div className="App-container--main">
-          <div className="App-container--main-head">
-            <h1>todo</h1>
-        
-            <label className="imgIcons" htmlFor="logoToggle">
-            <img src={sun} className="icon-sun" alt="sun-logo" />
-            <img className="icon-moon" src={moon} alt="sun-logo" />
-            </label>
-           
-          </div>
-          <div className="App-container--main-body">
-            <AddTodo AddTodoHandler={AddTodoHandler} />
-            <TodoList todosArray={todos} deleteIdHandler = {deleteIdHandler} />
+    <div>
+      <input type="checkbox" className="checkboxIn" id="logoToggle" />
+      <div className="App">
+        <div className="App-container">
+          <div className="App-container--main">
+            <div className="App-container--main-head">
+              <h1>todo</h1>
+
+              <label className="imgIcons" htmlFor="logoToggle">
+                <img src={sun} className="icon-sun" alt="sun-logo" />
+                <img className="icon-moon" src={moon} alt="sun-logo" />
+              </label>
+            </div>
+            <div className="App-container--main-body">
+              <AddTodo AddTodoHandler={AddTodoHandler} />
+              <TodoList todosArray={todos} deleteIdHandler={deleteIdHandler} />
+            </div>
           </div>
         </div>
+        <footer>
+          <p>Drag and drop to reoder list</p>
+        </footer>
       </div>
-      <footer>
-        <p>Drag and drop to reoder list</p>
-      </footer>
     </div>
-</div>
   );
 }
 
