ae3104bb10e30f441a56c18d8ed113f053d7abff
iddimsangi <adammsangi@gmail.com>
2021-12-13 14:05:50 +0300

    add toggle class functionality

--- src/components/TodoList/TodoCard/TodoCard.js
+++ src/components/TodoList/TodoCard/TodoCard.js
@@ -1,13 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import checkmark from "../../../images/icon-check.svg";
 import crossmark from "../../../images/icon-cross.svg";
 import "./TodoCard.scss";
 function TodoCard(props) {
   const { ID, todoDetail, isActive } = props.todosArrayObj;
+  const [isClicked, setisClicked] = useState(false);
+  const toggleClass = () => {
+    setisClicked(!isClicked);
+  };
   return (
     <div className="todo_card">
+      {/* <input type="checkbox" className="togglecircle" id="togglecircle" /> */}
       <div className="container">
-        <span className="circle">
+        <span onClick={toggleClass}  className={isClicked ?  "Active" : "circle" }>
           <img src={checkmark} alt="checkmark icon" />
         </span>
         <p>{todoDetail}</p>
